6c5b40d08c96a24e9d7f24fa4ef6a459cc2ef927
Eric Caron <eric.caron@gmail.com>
2021-03-07 00:33:04 -0600

    Rowers are rowing. Now to make positions right, charts show, saving work & loading previous saves

--- public/compete.js
+++ public/compete.js
@@ -1,28 +1,71 @@
-/* global $, Avataaars */
+/* global $, WebSocket, location */
 $(document).ready(function () {
-  let waterPos = 0; let waterSpeed = 10
+  let waterPos = 0; let waterSpeed = 100
+  let ws
   $('#startRace').on('click', function () {
+    const $water = $('#water')
+    const $rower = $('#water #rower')
+    const $competitor = $('#water #competitor')
+
     $('#starting-line').addClass('dropoff')
+
     setInterval(function () {
-      $('#water').css('background-position', '0 ' + waterPos + 'px')
+      $water.css('background-position', '0 ' + waterPos + 'px')
       waterPos += waterSpeed
-    }, 200)
+    }, 900)
+
     $('#startRace').fadeOut('slow', function () {
-      $('#speedUp').fadeIn('fast')
-      $('#speedDown').fadeIn('fast')
+      $('#toggles').fadeIn('fast')
     })
-  })
-  $('#speedUp').on('click', function () { waterSpeed += 10 })
-  $('#speedDown').on('click', function () { waterSpeed -= 10 })
-  $('#rower, #competitor').each(function () {
-    const avatar = $(this).data('avatar')
-    const options = {}
-    Object.keys(avatar).forEach(function (key) {
-      if (avatar[key]) {
-        options[key] = avatar[key]
+
+    const startTime = new Date()
+    const $timer = $('#timer h4')
+    setInterval(function () {
+      let curElapse = Math.round((new Date() - startTime) / 1000); let output = ''; let i
+      if (curElapse > 3600) {
+        i = Math.floor(curElapse / 3600)
+        output += i + ((i > 1) ? ' hours, ' : ' hour, ')
+        curElapse -= i * 3600
       }
-    })
-    const svg = Avataaars.create(options)
-    $(this).html(svg)
+      if (curElapse > 60 || output !== '') {
+        i = Math.floor(curElapse / 60)
+        output += i + ((i > 1) ? ' minutes, ' : ' minute, ')
+        curElapse -= i * 60
+      }
+      $timer.html(output + curElapse + ((curElapse === 1) ? ' second' : ' seconds'))
+    }, 500)
+
+    function connect () {
+      if (ws) {
+        ws.onerror = ws.onopen = ws.onclose = null
+        ws.close()
+      }
+
+      ws = new WebSocket(`ws://${location.host}`)
+      ws.onmessage = function (event) {
+        const data = JSON.parse(event.data)
+        $rower.css('bottom', Math.round(data.rower.position * 75) + '%')
+        $rower.find('.stats').html(`${data.rower.speed} ${data.rower.speedUnits}<br>${data.rower.distance} ${data.rower.distanceUnits}`)
+        $competitor.css('bottom', Math.round(data.competitor.position * 75) + '%')
+        $competitor.find('.stats').html(`${data.competitor.speed} ${data.competitor.speedUnits}<br>${data.competitor.distance} ${data.competitor.distanceUnits}`)
+      }
+      ws.onerror = function () {
+        console.log('WebSocket error')
+        ws.close()
+      }
+      ws.onopen = function () {
+        console.log('WebSocket connection established')
+        ws.send('Race Start!')
+      }
+      ws.onclose = function () {
+        console.log('WebSocket connection closed')
+        setTimeout(function () {
+          connect()
+        }, 1000)
+      }
+    }
+    connect()
   })
+  $('#speedUp').on('click', function () { waterSpeed = waterSpeed * 2 })
+  $('#speedDown').on('click', function () { waterSpeed = Math.ceil(waterSpeed / 2) })
 })
--- public/main.js
+++ public/main.js
@@ -1,81 +1,15 @@
-/* global $, fetch, location, Avataaars, WebSocket */
-(function () {
-  const messages = document.querySelector('#messages')
-  const wsButton = document.querySelector('#wsButton')
-  const wsSendButton = document.querySelector('#wsSendButton')
+/* global $, fetch, Avataaars */
+$(document).ready(function () {
   const logout = document.querySelector('#logout')
-  const login = document.querySelector('#login')
-
-  function showMessage (message) {
-    messages.textContent += `\n${message}`
-    messages.scrollTop = messages.scrollHeight
-  }
-
-  function handleResponse (response) {
-    return response.ok
-      ? response.json().then((data) => JSON.stringify(data, null, 2))
-      : Promise.reject(new Error('Unexpected response'))
-  }
-
-  if (login) {
-    login.onclick = function () {
-      fetch('/login', { method: 'POST', credentials: 'same-origin' })
-        .then(handleResponse)
-        .then(showMessage)
-        .catch(function (err) {
-          showMessage(err.message)
-        })
-    }
-  }
 
   if (logout) {
     logout.onclick = function () {
       fetch('/logout', { method: 'DELETE', credentials: 'same-origin' })
-        .then(handleResponse)
-        .then(showMessage)
-        .catch(function (err) {
-          if (err) console.warn(err)
+        .then(function () {
           window.location = '/'
         })
     }
   }
-
-  let ws
-  if (wsButton) {
-    wsButton.onclick = function () {
-      if (ws) {
-        ws.onerror = ws.onopen = ws.onclose = null
-        ws.close()
-      }
-
-      ws = new WebSocket(`ws://${location.host}`)
-      ws.onerror = function () {
-        showMessage('WebSocket error')
-      }
-      ws.onopen = function () {
-        showMessage('WebSocket connection established')
-      }
-      ws.onclose = function () {
-        showMessage('WebSocket connection closed')
-        ws = null
-      }
-    }
-  }
-
-  if (wsSendButton) {
-    wsSendButton.onclick = function () {
-      if (!ws) {
-        showMessage('No WebSocket connection')
-        return
-      }
-
-      ws.send('Hello World!')
-      showMessage('Sent "Hello World!"')
-    }
-  }
-})()
-
-$(document).ready(function () {
   $('.avatar').each(function () {
     const avatar = $(this).data('details')
     const options = {}
